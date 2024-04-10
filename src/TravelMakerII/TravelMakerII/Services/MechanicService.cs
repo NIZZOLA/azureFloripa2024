feec1df1cf826c3ad566a9b5c00319dc0073b11f
Marcio Rogerio Nizzola <marcio@nizzola.com.br>
2024-04-09 21:42:32 -0300

    modificação para nova versão da lib Azure Open Ai

---
+++ src/TravelMakerII/TravelMakerII/Services/MechanicService.cs
@@ -0,0 +1,35 @@
+﻿using Azure.AI.OpenAI;
+using Azure;
+using System.Text.Json;
+using TravelMakerII.Models;
+using Microsoft.Extensions.Options;
+using TravelMakerII.Interfaces;
+
+namespace TravelMakerII.Services;
+
+public class MechanicService : IMechanicService
+{
+    private readonly AzureAICredentials _config;
+    public MechanicService(IOptions<AzureAICredentials> options)
+    {
+        _config = options.Value;
+    }
+    public List<MechanicSolutionModel> GetMechanic(ProblemsRequestModel request)
+    {
+        OpenAIClient client = new(new Uri(_config.Endpoint), new AzureKeyCredential(_config.Key));
+
+        IList<ChatRequestMessage> messages = new List<ChatRequestMessage>();
+
+        messages.Add(new ChatRequestSystemMessage(PromptConstants.MechanicQuestion));
+        messages.Add(new ChatRequestSystemMessage(PromptConstants.JokeAnswer));
+        string question = $"tenho um veículo modelo: {request.VehicleModel} com o problema {request.Problem} ";
+
+        messages.Add(new ChatRequestUserMessage(question));
+
+        var chatCompletionsOptions = new ChatCompletionsOptions(_config.DeploymentModel, messages);
+
+        Response<ChatCompletions> response = client.GetChatCompletions(chatCompletionsOptions);
+
+        return JsonSerializer.Deserialize<List<MechanicSolutionModel>>(response.Value.Choices[0].Message.Content);
+    }
+}
